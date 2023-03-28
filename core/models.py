0d08defda54f7e1bd7772c63fc15af7dacf20e40
Ali Pourgheysari <a.geysar@gmail.com>
2023-03-28 17:37:02 +0430

    modified customUser fields
    made email, first name, and last name mandatory

--- core/models.py
+++ core/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 # create a CustomUser class that has an unique email field
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-
+    email = models.EmailField(unique=True, blank=False)
+    first_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
