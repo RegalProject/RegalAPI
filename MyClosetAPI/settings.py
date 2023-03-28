ea4ea900c457ba97c6b0d3058119376b503fe516
Alireza Karimi <alireza.mathers@gmail.com>
2023-03-28 16:22:54 +0330

    unique email
    made user email field unique

--- MyClosetAPI/settings.py
+++ MyClosetAPI/settings.py
@@ -113,6 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# use CustomUser as the default user model
+AUTH_USER_MODEL = 'core.CustomUser'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# create a CustomUser class that has an unique email field
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
