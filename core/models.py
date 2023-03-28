69ea9a09d02752e072264c67bc85abe846d5bfeb
Ali Pourgheysari <a.geysar@gmail.com>
2023-03-28 19:11:51 +0430

    added phone number to user

--- core/models.py
+++ core/models.py
@@ -5,5 +5,6 @@ from django.contrib.auth.models import AbstractUser
 # create a CustomUser class that has an unique email field
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
+    phone_number = models.CharField(max_length=15, blank=False, unique=True)
     first_name = models.CharField(max_length=150, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
