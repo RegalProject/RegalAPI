a380aaf8fe44ddca1a7079d1349c7a186f49f075
Alireza Karimi <alireza.mathers@gmail.com>
2023-03-29 23:30:15 +0330

    fixed debug media routing

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from . import serializers
 from . import models
 
+
 class ProfileViewSet(ModelViewSet):
     queryset = models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
+
