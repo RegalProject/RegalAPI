cd00ad291e6a9cdb03de402e6f07b3ef750fb16c
Alireza Karimi <alireza.mathers@gmail.com>
2023-05-31 23:58:41 +0330

    fixed some tests

--- closet/tests/test_recommended_item_api.py
+++ closet/tests/test_recommended_item_api.py
@@ -9,8 +9,37 @@ from closet.models import OwnedItem, CrawledItem, Type, Material, Occasion, Bran
 
 import os
 
+
 class RecommendedItemViewTest(CustomTestCase):
-    
+
+    def post_setUp(self):
+        self.image_content = open(os.path.join(settings.MEDIA_ROOT, 'ItemPics/download.jpg'), 'rb').read()
+
+        self.type = Type.objects.create(name='test type')
+        self.brand = Brand.objects.create(name='test brand')
+        self.material1 = Material.objects.create(name='test material 1')
+        self.material2 = Material.objects.create(name='test material 2')
+        self.occasion1 = Occasion.objects.create(name='test occasion 1')
+        self.occasion2 = Occasion.objects.create(name='test occasion 2')
+
+        self.crawled_item = CrawledItem.objects.create(
+            name='test',
+            season='Spring',
+            image='test.jpg',
+            color='test blue',
+            type=self.type,
+            brand=self.brand,
+            price=100,
+            url='https://test.com'
+        )
+
+        self.crawled_item.material.set([self.material1, self.material2])
+        self.crawled_item.occasion.set([self.occasion1, self.occasion2])
+
+    def setUp(self):
+        super().setUp()
+        self.post_setUp()
+
     def test_list_recommended_item_authenticated(self):
         url = reverse('recommendedItem-list')
         response = self.client.get(url)
@@ -22,7 +51,7 @@ class RecommendedItemViewTest(CustomTestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
-    def test_detail_recommended_item_authenticated(self):
+    def test_detail_recommended_item_authenticated(self):  # bug
         url = reverse('crawledItem-detail', kwargs={'pk': 1})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
--- closet/tests/test_wishlist_api.py
+++ closet/tests/test_wishlist_api.py
@@ -9,12 +9,33 @@ from closet.models import OwnedItem, CrawledItem, Type, Material, Occasion, Bran
 
 import os
 
+
 class WishlistViewTest(CustomTestCase):
-    
+
     def post_setUp(self):
-        pass
-        #to be fixed
-        
+        self.image_content = open(os.path.join(settings.MEDIA_ROOT, 'ItemPics/download.jpg'), 'rb').read()
+
+        self.type = Type.objects.create(name='test type')
+        self.brand = Brand.objects.create(name='test brand')
+        self.material1 = Material.objects.create(name='test material 1')
+        self.material2 = Material.objects.create(name='test material 2')
+        self.occasion1 = Occasion.objects.create(name='test occasion 1')
+        self.occasion2 = Occasion.objects.create(name='test occasion 2')
+
+        self.crawled_item = CrawledItem.objects.create(
+            name='test',
+            season='Spring',
+            image='test.jpg',
+            color='test blue',
+            type=self.type,
+            brand=self.brand,
+            price=100,
+            url='https://test.com'
+        )
+
+        self.crawled_item.material.set([self.material1, self.material2])
+        self.crawled_item.occasion.set([self.occasion1, self.occasion2])
+
     def setUp(self):
         super().setUp()
         self.post_setUp()
@@ -29,8 +50,27 @@ class WishlistViewTest(CustomTestCase):
         url = reverse('wishlist-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-    
+
+    def test_post_wishlist_authenticated(self):
+        url = reverse('wishlist-list')
+        response = self.client.post(url, {
+            'items': [1]
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_wishlist_unauthenticated(self):
+        self.client.force_authenticate(user=None, token=None)
+        url = reverse('wishlist-list')
+        response = self.client.post(url, {
+            'items': [1]
+        })
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_delete_wishlist_authenticated(self):
+        url = reverse('wishlist-list')
+        self.client.post(url, {
+            'items': [1]
+        })
         url = reverse('wishlist-detail', args=[1])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -39,20 +79,5 @@ class WishlistViewTest(CustomTestCase):
         self.client.force_authenticate(user=None, token=None)
         url = reverse('wishlist-detail', args=[1])
         response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        
-    def test_post_wishlist_authenticated(self):
-        url = reverse('wishlist-list')
-        response = self.client.post(url, {
-            'items': [1]
-        })
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
-    def test_post_wishlist_unauthenticated(self):
-        self.client.force_authenticate(user=None, token=None)
-        url = reverse('wishlist-list')
-        response = self.client.post(url, {
-            'items': [1]
-        })
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
