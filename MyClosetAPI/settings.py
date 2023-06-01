1fd5700cdf05d24cf9784d07da8f1961dc9d27c7
Alireza Karimi <alireza.mathers@gmail.com>
2023-06-01 17:13:31 +0330

    added static path

--- MyClosetAPI/settings.py
+++ MyClosetAPI/settings.py
@@ -170,11 +170,12 @@ AUTH_USER_MODEL_SERIALIZER = 'core.serializers.CustomUserSerializer'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
