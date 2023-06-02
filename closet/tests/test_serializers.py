b729f28c42c2dc4c4323cd7b336317a26604317e
Alireza Karimi <alireza.mathers@gmail.com>
2023-06-02 17:39:16 +0330

    added some serializer tests

--- closet/tests/test_serializers.py
+++ closet/tests/test_serializers.py
@@ -53,3 +53,82 @@ class SerializerTest(TestCase):
         self.assertEqual(serializer.data['brand'], self.brand.id)
         self.assertEqual(serializer.data['material'], [self.material1.id, self.material2.id])
         self.assertEqual(serializer.data['occasion'], [self.occasion1.id, self.occasion2.id])
+
+    def test_crawled_item_serializer(self):
+        crawled_item = models.CrawledItem.objects.create(
+            name='test',
+            season='test season',
+            image='test.jpg',
+            color='test color',
+            type=self.type,
+            brand=self.brand,
+            price=10000,
+            url='https://testurl.com'
+        )
+
+        crawled_item.material.set([self.material1, self.material2])
+        crawled_item.occasion.set([self.occasion1, self.occasion2])
+
+        serializer = serializers.CrawledItemSerializer(crawled_item)
+        self.assertEqual(serializer.data['name'], 'test')
+        self.assertEqual(serializer.data['season'], 'test season')
+        self.assertEqual(serializer.data['image'], '/media/test.jpg')
+        self.assertEqual(serializer.data['color'], 'test color')
+        self.assertEqual(serializer.data['typename'], 'test type')
+        self.assertEqual(serializer.data['brandname'], 'test brand')
+        self.assertEqual(serializer.data['materials'], ['test material 1', 'test material 2'])
+        self.assertEqual(serializer.data['occasions'], ['test occasion 1', 'test occasion 2'])
+        self.assertEqual(serializer.data['type'], self.type.id)
+        self.assertEqual(serializer.data['brand'], self.brand.id)
+        self.assertEqual(serializer.data['material'], [self.material1.id, self.material2.id])
+        self.assertEqual(serializer.data['occasion'], [self.occasion1.id, self.occasion2.id])
+        self.assertEqual(serializer.data['price'], 10000)
+        self.assertEqual(serializer.data['url'], 'https://testurl.com')
+
+    def test_owned_item_serializer(self):
+        owned_item = models.OwnedItem.objects.create(
+            name='test',
+            season='test season',
+            image='test.jpg',
+            color='test color',
+            type=self.type,
+            brand=self.brand,
+            owner=self.user,
+            score=76,
+            is_public=True
+        )
+
+        owned_item.material.set([self.material1, self.material2])
+        owned_item.occasion.set([self.occasion1, self.occasion2])
+
+        serializer = serializers.OwnedItemSerializer(owned_item, required=False)
+        self.assertEqual(serializer.data['name'], 'test')
+        self.assertEqual(serializer.data['season'], 'test season')
+        self.assertEqual(serializer.data['image'], '/media/test.jpg')
+        self.assertEqual(serializer.data['color'], 'test color')
+        self.assertEqual(serializer.data['typename'], 'test type')
+        self.assertEqual(serializer.data['brandname'], 'test brand')
+        self.assertEqual(serializer.data['materials'], ['test material 1', 'test material 2'])
+        self.assertEqual(serializer.data['occasions'], ['test occasion 1', 'test occasion 2'])
+        self.assertEqual(serializer.data['type'], self.type.id)
+        self.assertEqual(serializer.data['brand'], self.brand.id)
+        self.assertEqual(serializer.data['material'], [self.material1.id, self.material2.id])
+        self.assertEqual(serializer.data['occasion'], [self.occasion1.id, self.occasion2.id])
+        self.assertEqual(serializer.data['score'], 76)
+        self.assertEqual(serializer.data['is_public'], True)
+
+    def test_brand_serializer(self):
+        serializer = serializers.BrandSerializer(self.brand)
+        self.assertEqual(serializer.data['name'], 'test brand')
+
+    def test_material_serializer(self):
+        serializer = serializers.MaterialSerializer(self.material1)
+        self.assertEqual(serializer.data['name'], 'test material 1')
+
+    def test_occasion_serializer(self):
+        serializer = serializers.OccasionSerializer(self.occasion1)
+        self.assertEqual(serializer.data['name'], 'test occasion 1')
+
+    def test_type_serializer(self):
+        serializer = serializers.TypeSerializer(self.type)
+        self.assertEqual(serializer.data['name'], 'test type')
